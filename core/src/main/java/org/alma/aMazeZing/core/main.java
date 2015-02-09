98c2544128eeb6acbfbcbec63e8c22a47e8bc563
Dralagen <dralagen@dralagen.fr>
2015-02-09 18:03:01 +0100

    add archi with module core

---
+++ core/src/main/java/org/alma/aMazeZing/core/main.java
@@ -0,0 +1,13 @@
+package org.alma.aMazeZing.core;
+
+/**
+ * Created on 2/9/15.
+ *
+ * @author dralagen
+ */
+public class main {
+
+    public static void main (String[] args) {
+        System.out.println("Hello World!");
+    }
+}
